e531f4e426b6d6233adc63835487f6aefa901ec1
sean-cho <soonwengcho@gmail.com>
2015-02-24 20:16:16 -0500

    Build toolkit package. Init.

---
+++ R/makecolors.R
@@ -0,0 +1,66 @@
+#' Make color labels for factors/categorical data.
+#' 
+#' \code{makecolors} Generates color labels and legends for factors.
+#' 
+#' @param object A matrix of vector that can be coerced into factors.
+#' @param pal A vector of colors.
+#' @param keep.order Logical. Keep original order of non-factor strings.
+#' @details The function accepts either a vector that can be coerced into 
+#' factors or a matrix where each \strong{column} is used to generate color 
+#' labels.
+#' 
+#' Can also be generate unique \code{pch} labels (see \bold{Examples}).
+#' 
+#' @return A list containing:
+#' \describe{
+#'  \item{Legend:}{A unique vector/color pair for easy legend plotting.}
+#'  \item{Colors:}{A matrix or vector of colors for plotting.}
+#'  }
+#' @examples
+#' x <- factor(sample(c('a','b','c'), 30, replace = TRUE))
+#' colset <- makecolors(x)
+#' xnum <- jitter(as.numeric(x))
+#' plot(xnum, col = colset$Colors, pch = 16)
+#' legend('topright', legend = names(colset$Legend), col = colset$Legend, pch = 16)
+#' 
+#' pchset <- makecolors(x, pal = c(16,17,18))$Colors
+#' plot(xnum, col = colset$Colors, pch = pchset)
+
+makecolors = function(object,pal=cbbPalette,keep.order=TRUE) {
+  require(RColorBrewer)
+  if(is.null(dim(object))){
+    ANS = list()
+    CLfactor = inherits(object,"factor")
+    if(keep.order & CLfactor) {dum.order = levels(object)}
+    object = as.character(object)
+    tempPal = pal
+    obj.unique = unique(object)
+    obj.colors = tempPal[1:length(obj.unique)]
+    names(obj.colors) = obj.unique
+    color.set = obj.colors[object]
+    names(color.set) = object
+    if(keep.order & CLfactor) {obj.colors = obj.colors[dum.order]}
+    ANS$Legend = obj.colors
+    ANS$Colors = color.set
+    return(ANS)
+  }
+  if(is.null(dim(object))==FALSE){
+    color.set.mat = matrix(NA,nrow(object),ncol(object))
+    legend.list = list()
+    tempPal = pal
+    for(i in 1:ncol(object)){
+      object.dum = apply(object,2,as.character)
+      obj.unique = unique(object.dum[,i])
+      obj.colors = tempPal[1:length(obj.unique)]
+      names(obj.colors) = obj.unique
+      color.set.mat[,i] = obj.colors[object.dum[,i]]
+      legend.list[[i]] = obj.colors
+    }
+    colnames(color.set.mat) = colnames(object)
+    rownames(color.set.mat) = rownames(object)
+    names(legend.list) = colnames(object)
+    ANS = list(Legend=legend.list,Colors=color.set.mat)
+    return(ANS)
+  }
+}
+
---
+++ R/myheatmap2.R
@@ -0,0 +1,23 @@
+#' Personalized heatmap.2
+#' 
+#' \code{myheatmap2} Runs \code{heatmap.2} with personalized color scheme.
+#' 
+#' @param x Numeric matrix of values to be plotted. Data.frame will be coerced.
+#' @param col Defaults to a blue-black-yellow color scheme with 50 shades
+#' @param ... Other arguments to be passed on to \code{heatmap.2}
+#' 
+#' @details Turns of \code{density.info} and \code{trace} and sets the
+#' color scheme to blue-black-yellow. Other arguments can be passed onto
+#' \code{heatmap.2}. Most commonly used are \code{ColSideCol} and 
+#' \code{RowSideCol}.
+#' 
+#' @seealso \code{\link[gplots]{heatmap.2}}
+
+myheatmap2 = function(x,col=colorRampPalette(c("blue","black","yellow"))(50),...){
+  if(!requireNamespace('gplots', quietly = TRUE)){
+    stop('Package gplots needed for this function. Please install it', call. = FALSE)
+  }
+  heatmap.2(as.matrix(x),density.info="none",
+            col=col,
+            trace="none",...)
+}
