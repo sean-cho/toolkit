aae38663fe2c62d9673cb3c2fd598daf17bae79b
sean-cho <soonwengcho@gmail.com>
2015-05-04 20:02:58 -0400

    Add heatmap.3 and companion functions.

---
+++ R/myheatmap3.R
@@ -0,0 +1,25 @@
+#' Personalized heatmap.2
+#' 
+#' \code{myheatmap3} Runs \code{heatmap.3} with personalized color scheme.
+#' 
+#' @param x Numeric matrix of values to be plotted. Data.frame will be coerced.
+#' @param col Defaults to a blue-black-yellow color scheme with 50 shades
+#' @param ... Other arguments to be passed on to \code{heatmap.3}
+#' 
+#' @details Turns of \code{density.info} and \code{trace} and sets the
+#' color scheme to blue-black-yellow. Other arguments can be passed onto
+#' \code{heatmap.3}. Most commonly used are \code{ColSideCol} and 
+#' \code{RowSideCol}.
+#' 
+#' @return An object of the class list from \code{heatmap.3}.
+#' @export
+#' 
+#' @seealso \code{\link[gplots]{heatmap.3}}
+#' 
+#' @importFrom gplots heatmap.3
+
+myheatmap3 = function(x,col=colorRampPalette(c("blue","black","yellow"))(50),...){
+  heatmap.3(as.matrix(x),density.info="none",
+            col=col,
+            trace="none",...)
+}
---
+++ R/unsupervised3.R
@@ -0,0 +1,25 @@
+#' Unsupervised heatmap by standard deviation
+#' 
+#' \code{unsupervised.3} Performs unsupervised clustering on the most
+#'    variable features using heatmap.3 which allows for multiple Row/ColSideCol.
+#'    
+#' @param x Numeric matrix with samples in columns and features in rows.
+#' @param n Number of features
+#' @param col Color for heatmap. Defaults to blue-black-yellow color scheme.
+#' @param ... Passes arguments to \code{heatmap.3}
+#' 
+#' @details Finds the most variable features across all samples by standard
+#'    deviation, clusters them, and plots it on a heatmap using the
+#'    \code{heatmap.3} function.
+#'    
+#' @return A heatmap.3 object and plot.
+#' @export
+#'    
+#' @seealso \code{\link[gplots]{heatmap.2}}
+
+unsupervised.3 = function(x,n,col=colorRampPalette(c("blue","black","yellow"))(50),...){
+  dumsd = apply(x,1,sd,na.rm=TRUE)
+  dumrows = dim(x)[1]
+  dum = x[dumsd>=quantile(dumsd,(dumrows-n)/dumrows,na.rm=TRUE),]
+  myheatmap3(dum,main=paste("Top",round(n),"Probes",sep=" "),col = col,...)
+}
